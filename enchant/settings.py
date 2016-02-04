f3a7a1e3c3a6e2e13572a549b7a875e90c562e25
julian.trutmann <jtrutmann@gmail.com>
2016-02-04 18:50:51 -0500

    Mode Settings Tweeks.
    More settings tweeks for database stuff.

--- enchant/settings.py
+++ enchant/settings.py
@@ -104,9 +104,10 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': dj_database_url.config()
 }
-DATABASES['default'] =  dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+# DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 
 # Internationalization
