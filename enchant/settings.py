a4d4e3d8502680823517736004e29530186eae38
julian.trutmann <jtrutmann@gmail.com>
2016-02-04 18:47:08 -0500

    Settings Changes For Database
    Made a small settings change to try and get the database working.

--- enchant/settings.py
+++ enchant/settings.py
@@ -99,12 +99,12 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+}
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
